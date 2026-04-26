ad07c62c9f2f8c600786f04a79b82e9a3872d65e
sordillajenus@gmail.com <sordillajenus@gmail.com>
2026-04-26 14:12:34 +0800

    The program is working

--- program_3/file_handling_method_in_python.py
+++ program_3/file_handling_method_in_python.py
@@ -3,7 +3,7 @@
 
 with open("mylife.txt", "a") as file:
     while True:
-        content = input("write something about your day: ")
+        content = input("Write something about your day: ")
         file.write(content + "\n")
         
         question = input("Are there more lines? yes/no: ")
