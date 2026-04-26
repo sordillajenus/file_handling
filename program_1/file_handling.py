03b6ef5e193b0fff1a64a60559c4c5d7974b9677
sordillajenus@gmail.com <sordillajenus@gmail.com>
2026-04-26 09:40:10 +0800

    opening numbers, even and odd txt file. Looping each line from numbers.txt for the extraction

--- program_1/file_handling.py
+++ program_1/file_handling.py
@@ -1,2 +1,13 @@
-file = open("numbers.txt", "w")
-file.close()
+# file = open("numbers.txt", "w")
+# file.close()
+
+with open("numbers.txt", "r") as file_1, \
+     open("even.txt", "w") as file_2, \
+     open("odd.txt", "w") as file_3:
+    
+    for line in file_1:
+        line = line.strip()
+        if line == " ":
+            continue
+
+    
