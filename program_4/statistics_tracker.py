67b094669fb63ea512ff201c10be10d2bd337888
sordillajenus@gmail.com <sordillajenus@gmail.com>
2026-04-26 19:16:52 +0800

    i created a separate class that will track the statistics of every number and creates a summary report.

---
+++ program_4/statistics_tracker.py
@@ -0,0 +1,15 @@
+class StatisticsTracker:
+    def __init__ (self, even_file, odd_file, statistics_file, summary_report_file):
+        self.even_file = even_file
+        self.odd_file = odd_file
+        self.statistics_file = statistics_file
+        self.summary_report_file = summary_report_file
+
+    def generate_statistical_analysis():
+
+        even = []
+        odd = []
+
+        with open()
+
+
