cabc73ad3970333a6b9308450c4b661a70668999
sordillajenus@gmail.com <sordillajenus@gmail.com>
2026-04-26 14:44:12 +0800

    unfinished block of code which is to read the integers.txt file and extract the squared and cubed value of every integers

--- program_4/file_handling_integers.py
+++ program_4/file_handling_integers.py
@@ -1,4 +1,17 @@
 # file = open("integers_1.txt", "w")
 # file.close()
 
-with open("integers_1")
+with open("integers_1.txt", "r") as file_1, \
+     open("double.txt", "w") as file_2, \
+     open("triple.txt", "w") as file_3:
+    
+    for line in file_1:
+        line = line.strip()
+        if line == " ":
+            continue
+
+        num = int(line)
+
+        if num >= 0:
+            num ** 2
+            file
