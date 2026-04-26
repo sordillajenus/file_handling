07e9fb9bdcf933acff3133a277f2b02c08f620fa
sordillajenus@gmail.com <sordillajenus@gmail.com>
2026-04-26 19:23:35 +0800

    separate def function for even and odd files

--- program_4/statistics_tracker.py
+++ program_4/statistics_tracker.py
@@ -1,15 +1,27 @@
 class StatisticsTracker:
-    def __init__ (self, even_file, odd_file, statistics_file, summary_report_file):
+    def __init__ (self, even_file, odd_file, summary_report_file):
         self.even_file = even_file
         self.odd_file = odd_file
-        self.statistics_file = statistics_file
         self.summary_report_file = summary_report_file
 
-    def generate_statistical_analysis():
+        def generate_summary(self):
+            even_numbers = []
+            odd_numbers = []
 
-        even = []
-        odd = []
+        
+            with open(self.even_file, "r") as file_1:
+                for line in file_1:
+                    line = line.strip()
+                    if line:
+                        even_numbers.append(int(line))
 
-        with open()
+            with open(self.odd_file, "r") as file_2:
+                for line in file_2:
+                    line = line.strip()
+                    if line:
+                        odd_numbers.append(int(line))
+
+                    
+
 
 
