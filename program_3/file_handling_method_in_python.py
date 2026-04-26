628f8e4b96f31f251e150c5cfc325dd2d6650f68
sordillajenus@gmail.com <sordillajenus@gmail.com>
2026-04-26 13:53:51 +0800

    test block of code whether to see if input functions works with file with an append permission

--- program_3/file_handling_method_in_python.py
+++ program_3/file_handling_method_in_python.py
@@ -1,2 +1,8 @@
-file = open("mylife.txt", "w")
-file.close()
+# file = open("mylife.txt", "w")
+# file.close()
+
+with open("mylife.txt", "a") as file:
+    while True:
+        content = input("Write a line from your diary: ")
+        file.append(content)
+
