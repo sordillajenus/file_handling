55bdf0574237d3017f6cadfc8c565863995b09e5
sordillajenus@gmail.com <sordillajenus@gmail.com>
2026-04-26 11:24:01 +0800

    the complete block of code. So basically i tried making a placeholder since name and gwa would return attribute error. So everytime the program loops, it will also compare the current gwa he is looping to the placeholder gwa that i created which i set to infinity so everytime the program loops a gwa that is smaller (because that's how gwa is computed) it will automatically store the name hes looping and the gwa to the placeholder i created. It doesnt matter wheter the loop continues because it will always check if the gwa is lower to the current gwa stored in my placeholder. Hence, finishing the final block of code.

--- program_2/file_handling_gwa.py
+++ program_2/file_handling_gwa.py
@@ -1,11 +1,27 @@
 # file = open("students_with_their_gwa.txt", "w")
 # file.close()
 
+top_student = " "
+top_gwa = float("inf")
+
 with open("students_with_their_gwa.txt", "r") as file:
     for line in file:
        if "-" in line:
-        line.split("-")
+        new_line = line.split("-")
+        name = new_line[0].strip()
+        gwa = float(new_line[1].strip())
         
+        if gwa < top_gwa:
+           top_student = name
+           top_gwa = gwa
+
+print(f"The student with the highest gwa is {top_student} with the general weighted average of {top_gwa}")
+
+        
+           
+
+        
+
 
 
 
