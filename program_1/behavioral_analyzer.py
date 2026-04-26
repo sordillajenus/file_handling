dd34a95dd6f563bab2aef2f448a5119acaf7f6ff
sordillajenus@gmail.com <sordillajenus@gmail.com>
2026-04-26 23:45:31 +0800

    i created a new class that will analyze the texts and will make a report on how the values behaved and their significance and relationship.

---
+++ program_1/behavioral_analyzer.py
@@ -0,0 +1,7 @@
+class BehavioralAnalyzer:
+    def __init__ (self, number_file, even_file, odd_file, analysis_file):
+        self.number_file = number_file
+        self.even_file = even_file
+        self.odd_file = odd_file
+        self.analysis_file = analysis_file
+        
