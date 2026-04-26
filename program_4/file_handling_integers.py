68d569b89bb9e59ea7b10dfb17d3ae0a644967ec
sordillajenus@gmail.com <sordillajenus@gmail.com>
2026-04-26 18:24:36 +0800

    i forgot that the program should be OOP so i set up a class and objects to call

--- program_4/file_handling_integers.py
+++ program_4/file_handling_integers.py
@@ -1,14 +1,18 @@
-file = open("integers_1.txt", "w")
-file.close()
+class IntegersEvenOdd:
+    def __init__ (self, integer_file, even_file, odd_file):
+        self.integer_file = integer_file
+        self.even_file = even_file
+        self.odd_file = odd_file
 
-with open("integers_1.txt", "r") as file_1, \
-     open("double.txt", "w") as file_2, \
-     open("triple.txt", "w") as file_3:
+    def process(self):
+        with open("integers_1.txt", "r") as file_1, \
+            open("double.txt", "w") as file_2, \
+            open("triple.txt", "w") as file_3:
     
-    for line in file_1:
-        line = line.strip()
-        if line == " ":
-            continue
+            for line in file_1:
+                line = line.strip()
+                if line == " ":
+                    continue
 
         num = int(line)
 
