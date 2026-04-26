76680cadfe1b928776c8eec4e7c92a1c7de35598
sordillajenus@gmail.com <sordillajenus@gmail.com>
2026-04-26 22:37:20 +0800

    transforming the program into OOP (final block of code)

--- program_1/file_handling.py
+++ program_1/file_handling.py
@@ -1,21 +1,32 @@
 class IntegerManipulation:
-    def __init__ (self, number_file, even_file, odd_file):
+    def __init__(self, number_file, even_file, odd_file):
         self.number_file = number_file
         self.even_file = even_file
         self.odd_file = odd_file
 
-    with open(self.number_file, "r") as file_1, \
-        open(self.even_file, "w") as file_2, \
-        open(self.odd_file, "w") as file_3:
-    
-        for line in file_1:
-            line = line.strip()
-            if line == " ":
-                continue
+    def process(self):
+        with open(self.number_file, "r") as file_1, \
+             open(self.even_file, "w") as file_2, \
+             open(self.odd_file, "w") as file_3:
 
-            num = int(line)
+            for line in file_1:
+                line = line.strip()
 
-            if num % 2 == 0:
-                file_2.write(str(num) + "\n")
-            else:
-                file_3.write(str(num) + "\n")
+                if line == "":
+                    continue
+
+                num = int(line)
+
+                if num % 2 == 0:
+                    file_2.write(str(num) + "\n")
+                else:
+                    file_3.write(str(num) + "\n")
+
+
+integer = IntegerManipulation(
+    "numbers.txt",
+    "even.txt",
+    "odd.txt"
+)
+
+integer.process()
