971c55374728672677d70912948fb3492d15f3e8
sordillajenus@gmail.com <sordillajenus@gmail.com>
2026-04-27 16:12:16 +0800

    i fixed majority of the logics because the last txt file was deleted

--- program_2/file_handling_gwa.py
+++ program_2/file_handling_gwa.py
@@ -1,14 +1,11 @@
 class StudentGWAAnalyzer:
-    def __init__(self, filename):
+    def __init__(self, filename, output_file):
         self.filename = filename
+        self.output_file = output_file
         self.top_student = " "
         self.top_gwa = float("inf")
 
-    def create_file(self):
-        file = open(self.filename, "w")
-        file.close()
-
-    def find_top_student(self):
+    def analyze(self):
         with open(self.filename, "r") as file:
             for line in file:
                 if "-" in line:
@@ -20,12 +17,22 @@ class StudentGWAAnalyzer:
                         self.top_student = name
                         self.top_gwa = gwa
 
-    def display_result(self):
-        print(f"The student with the highest GWA is {self.top_student} with the general weighted average of {self.top_gwa}")
+    def save_result(self):
+        with open(self.output_file, "w") as file:
+            file.write("STUDENT GWA ANALYSIS\n\n")
+            file.write(f"Top Student: {self.top_student}\n")
+            file.write(f"Highest Performance (Lowest GWA): {self.top_gwa}\n")
+
+    def display(self):
+        print(f"The student with the highest GWA is {self.top_student} with a GWA of {self.top_gwa}")
 
 
-analyzer = StudentGWAAnalyzer("students_with_their_gwa.txt")
 
-analyzer.create_file()
-analyzer.find_top_student()
-analyzer.display_result()
+analyzer = StudentGWAAnalyzer(
+    "students_with_their_gwa.txt",
+    "gwa_analysis_report.txt"
+)
+
+analyzer.analyze()
+analyzer.save_result()
+analyzer.display()
