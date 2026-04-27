7e3ffa66ee59c60c7defa2450fe35a2d15bbf1e9
sordillajenus@gmail.com <sordillajenus@gmail.com>
2026-04-27 15:58:56 +0800

    i modifed everything in the 2nd program to transform it into oop

--- program_2/file_handling_gwa.py
+++ program_2/file_handling_gwa.py
@@ -1,27 +1,26 @@
-file = open("students_with_their_gwa.txt", "w")
-file.close()
+class StudentGWAAnalyzer:
+    def __init__(self, filename):
+        self.filename = filename
+        self.top_student = " "
+        self.top_gwa = float("inf")
 
-top_student = " "
-top_gwa = float("inf")
+    def create_file(self):
+        file = open(self.filename, "w")
+        file.close()
 
-with open("students_with_their_gwa.txt", "r") as file:
-    for line in file:
-       if "-" in line:
-        new_line = line.split("-")
-        name = new_line[0].strip()
-        gwa = float(new_line[1].strip())
-        
-        if gwa < top_gwa:
-           top_student = name
-           top_gwa = gwa
+    def find_top_student(self):
+        with open(self.filename, "r") as file:
+            for line in file:
+                if "-" in line:
+                    new_line = line.split("-")
+                    name = new_line[0].strip()
+                    gwa = float(new_line[1].strip())
 
-print(f"The student with the highest GWA is {top_student} with the general weighted average of {top_gwa}")
+                    if gwa < self.top_gwa:
+                        self.top_student = name
+                        self.top_gwa = gwa
 
-        
-           
-
-        
-
-
+    def display_result(self):
+        print(f"The student with the highest GWA is {self.top_student} with the general weighted average of {self.top_gwa}")
 
 
