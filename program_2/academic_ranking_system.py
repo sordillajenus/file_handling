2585286d2bc49908015daa2d7000c777bc416cbe
sordillajenus@gmail.com <sordillajenus@gmail.com>
2026-04-27 20:51:11 +0800

    i created a def function that will rank the students based on their general weighted average

--- program_2/academic_ranking_system.py
+++ program_2/academic_ranking_system.py
@@ -1,2 +1,33 @@
 class AcademicInsightAnalysis:
-    pass
+    def __init__ (self, file_name, academic_report):
+        self.file_name = file_name
+        self.academic_report = academic_report
+
+    def ranking(self):
+        with open(self.file_name, "r") as file_1, \
+            open(self.academic_report, "w") as file_2:
+
+            students_ranking = {
+
+            }
+            for line in file_1:
+                lines = line.split("-")
+                names = lines[0].strip()
+                average = float(lines[1].strip())
+                students_ranking[names] = average
+
+            rank = sorted(students_ranking.items(), key = lambda x: x[1])
+            rank_number = 1
+            file_2.write("Below is the ranking of students who have demonstrated exemplary academic performance. Their dedication, effort, and commitment have led them to achieve outstanding grades.\n\n")            
+            for key, value in rank:
+                file_2.write(f"Rank {rank_number}: {key} - {value}\n")
+                rank_number += 1
+
+
+                
+
+
+
+
+
+    
