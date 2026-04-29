e7ac7a3e7323f13bff9ecf47488034afed890a19
sordillajenus@gmail.com <sordillajenus@gmail.com>
2026-04-29 10:17:03 +0800

    i transposed the remainning block of code into OOP and new methods (still unfinished)

--- program_3/file_handling_method_in_python.py
+++ program_3/file_handling_method_in_python.py
@@ -3,17 +3,17 @@ class MyLifeWriter:
         self.file_name = filename
 
     def initialize_file(self):
-        with open(self.file_name, "w", encoding="utf-8")
+        with open(self.file_name, "w", encoding="utf-8"):
             pass
 
+    def write_entries(self):
+        with open(self.file_name, "w", encoding="utf-8") as file:
+         
+         while True:
+            content = input("Write something about your day: ")
+            file.write(content + "\n")
 
-file = open("mylife.txt", "w")
-file.close()
-
-with open("mylife.txt", "a") as file:
-    while True:
-        content = input("Write something about your day: ")
-        file.write(content + "\n")
+            while True:
         
         question = input("Are there more lines? yes/no: ")
         if question not in ["yes", "no"]:
