060ba7a62da238461dd238d8da2a678552794d0d
sordillajenus@gmail.com <sordillajenus@gmail.com>
2026-04-29 11:40:17 +0800

    i created a function for writing entry

--- program_3/life_analytics.py
+++ program_3/life_analytics.py
@@ -4,3 +4,26 @@ from collections import Counter
 class LifeAnalytics:
     def __init__ (self, filename):
         self.filename = filename
+
+    def write_entry(self):
+        with open (self.filename, "a") as file:
+
+            while True:
+                mood = input("What's your mood today?: ")
+                entry = input("write about your day today: ")
+
+                word_count = len(entry.split())
+                timestamp = datetime.datetime.now()
+
+                file.write(f"[{timestamp}]\n")
+                file.write(f"Mood: {mood}\n")
+                file.write(f"Words: {word_count}\n")
+                file.write(f"Entry: {entry}\n\n")
+
+                count = input("Add another line? yes/no: ").lower
+                if count == "no":
+                    break
+
+    def analyze_entries(self):
+        mood = []
+        word = []
